75be8124e91818fff720dde062716cfbdb41b011
maxgerhardt <maximilian.gerhardt@rub.de>
2023-02-25 15:00:13 +0100

    Format code

--- examples/blinky-noneos/src/main.c
+++ examples/blinky-noneos/src/main.c
@@ -1,10 +1,10 @@
 #include <ch32v30x.h>
 
-#define BLINKY_GPIO_PORT   GPIOA
-#define BLINKY_GPIO_PIN    GPIO_Pin_1
+#define BLINKY_GPIO_PORT GPIOA
+#define BLINKY_GPIO_PIN GPIO_Pin_1
 #define BLINKY_CLOCK_EABLE RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE)
 
-static uint8_t  p_us = 0;
+static uint8_t p_us = 0;
 static uint16_t p_ms = 0;
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -18,18 +18,18 @@ int main(void)
 	SystemCoreClockUpdate();
 	Delay_Init();
 
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
+	GPIO_InitTypeDef GPIO_InitStructure = {0};
 
-    BLINKY_CLOCK_EABLE;
-    GPIO_InitStructure.GPIO_Pin = BLINKY_GPIO_PIN;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(BLINKY_GPIO_PORT, &GPIO_InitStructure);
+	BLINKY_CLOCK_EABLE;
+	GPIO_InitStructure.GPIO_Pin = BLINKY_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(BLINKY_GPIO_PORT, &GPIO_InitStructure);
 
 	uint8_t ledState = 0;
-	while(1)
-    {
-        GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, ledState);
+	while (1)
+	{
+		GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, ledState);
 		ledState ^= 1; // invert for the next run
 		Delay_Ms(1000);
 	}
@@ -37,26 +37,30 @@ int main(void)
 
 void Delay_Init(void)
 {
-    p_us = SystemCoreClock / 8000000;
-    p_ms = (uint16_t)p_us * 1000;
+	p_us = SystemCoreClock / 8000000;
+	p_ms = (uint16_t)p_us * 1000;
 }
 
 void Delay_Ms(uint32_t n)
 {
-    uint32_t i;
+	uint32_t i;
 
-    SysTick->SR &= ~(1 << 0);
-    i = (uint32_t)n * p_ms;
+	SysTick->SR &= ~(1 << 0);
+	i = (uint32_t)n * p_ms;
 
-    SysTick->CMP = i;
-    SysTick->CTLR |= (1 << 4);
-    SysTick->CTLR |= (1 << 5) | (1 << 0);
+	SysTick->CMP = i;
+	SysTick->CTLR |= (1 << 4);
+	SysTick->CTLR |= (1 << 5) | (1 << 0);
 
-    while((SysTick->SR & (1 << 0)) != (1 << 0))
-        ;
-    SysTick->CTLR &= ~(1 << 0);
+	while ((SysTick->SR & (1 << 0)) != (1 << 0))
+		;
+	SysTick->CTLR &= ~(1 << 0);
 }
 
-void NMI_Handler(void) { }
-void HardFault_Handler(void) { while (1) {} }
-
+void NMI_Handler(void) {}
+void HardFault_Handler(void)
+{
+	while (1)
+	{
+	}
+}
