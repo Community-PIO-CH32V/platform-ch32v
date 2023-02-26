13b241a40cfd5a3d66c9aaa97b1d6dff20d25759
maxgerhardt <maximilian.gerhardt@rub.de>
2023-02-26 15:20:15 +0100

    Format FreeRTOS config properly

--- examples/blinky-freertos/src/FreeRTOSConfig.h
+++ examples/blinky-freertos/src/FreeRTOSConfig.h
@@ -87,57 +87,57 @@
 /* See https://www.freertos.org/Using-FreeRTOS-on-RISC-V.html */
 
 /* don't have MTIME */
-#define configMTIME_BASE_ADDRESS 	 ( 0 )
+#define configMTIME_BASE_ADDRESS     ( 0 )
 #define configMTIMECMP_BASE_ADDRESS  ( 0 )
 
-#define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				0
-#define configUSE_TICK_HOOK				0
-#define configCPU_CLOCK_HZ				SystemCoreClock
-#define configTICK_RATE_HZ				( ( TickType_t ) 500 )
-#define configMAX_PRIORITIES			( 15 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 256 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 12 * 1024 ) )
-#define configMAX_TASK_NAME_LEN			( 16 )
-#define configUSE_TRACE_FACILITY		0
-#define configUSE_16_BIT_TICKS			0
-#define configIDLE_SHOULD_YIELD			0
-#define configUSE_MUTEXES				1
-#define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	0
-#define configUSE_RECURSIVE_MUTEXES		1
-#define configUSE_MALLOC_FAILED_HOOK	0
-#define configUSE_APPLICATION_TASK_TAG	0
-#define configUSE_COUNTING_SEMAPHORES	1
-#define configGENERATE_RUN_TIME_STATS	0
+#define configUSE_PREEMPTION            1
+#define configUSE_IDLE_HOOK             0
+#define configUSE_TICK_HOOK             0
+#define configCPU_CLOCK_HZ              SystemCoreClock
+#define configTICK_RATE_HZ              ( ( TickType_t ) 500 )
+#define configMAX_PRIORITIES            ( 15 )
+#define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 256 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
+#define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 12 * 1024 ) )
+#define configMAX_TASK_NAME_LEN         ( 16 )
+#define configUSE_TRACE_FACILITY        0
+#define configUSE_16_BIT_TICKS          0
+#define configIDLE_SHOULD_YIELD         0
+#define configUSE_MUTEXES               1
+#define configQUEUE_REGISTRY_SIZE       8
+#define configCHECK_FOR_STACK_OVERFLOW  0
+#define configUSE_RECURSIVE_MUTEXES     1
+#define configUSE_MALLOC_FAILED_HOOK    0
+#define configUSE_APPLICATION_TASK_TAG  0
+#define configUSE_COUNTING_SEMAPHORES   1
+#define configGENERATE_RUN_TIME_STATS   0
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 			0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_CO_ROUTINES             0
+#define configMAX_CO_ROUTINE_PRIORITIES   ( 2 )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS				1
-#define configTIMER_TASK_PRIORITY		( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH		4
-#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE )
+#define configUSE_TIMERS                1
+#define configTIMER_TASK_PRIORITY       ( configMAX_PRIORITIES - 1 )
+#define configTIMER_QUEUE_LENGTH        4
+#define configTIMER_TASK_STACK_DEPTH    ( configMINIMAL_STACK_SIZE )
 
 
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet			1
-#define INCLUDE_uxTaskPriorityGet			1
-#define INCLUDE_vTaskDelete					1
-#define INCLUDE_vTaskCleanUpResources		1
-#define INCLUDE_vTaskSuspend				1
-#define INCLUDE_vTaskDelayUntil				1
-#define INCLUDE_vTaskDelay					1
-#define INCLUDE_eTaskGetState				1
-#define INCLUDE_xTimerPendFunctionCall		1
-#define INCLUDE_xTaskAbortDelay				1
-#define INCLUDE_xTaskGetHandle				1
-#define INCLUDE_xSemaphoreGetMutexHolder	1
+#define INCLUDE_vTaskPrioritySet           1
+#define INCLUDE_uxTaskPriorityGet          1
+#define INCLUDE_vTaskDelete                1
+#define INCLUDE_vTaskCleanUpResources      1
+#define INCLUDE_vTaskSuspend               1
+#define INCLUDE_vTaskDelayUntil            1
+#define INCLUDE_vTaskDelay                 1
+#define INCLUDE_eTaskGetState              1
+#define INCLUDE_xTimerPendFunctionCall     1
+#define INCLUDE_xTaskAbortDelay            1
+#define INCLUDE_xTaskGetHandle             1
+#define INCLUDE_xSemaphoreGetMutexHolder   1
 
 
 /* Normal assert() semantics without relying on the provision of an assert.h
