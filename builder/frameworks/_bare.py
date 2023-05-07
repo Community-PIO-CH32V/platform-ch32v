80e66aef9a8722e73d20db79adfa99a087277612
maxgerhardt <maximilian.gerhardt@rub.de>
2023-05-07 13:14:12 +0200

    Second try

--- builder/frameworks/_bare.py
+++ builder/frameworks/_bare.py
@@ -15,6 +15,7 @@ data_limit = 0 if mcu.startswith("ch32v0") else 8
 machine_arch = board.get("build.march")
 # "fix" Mac toolchain being incapable of linking against libgcc
 # by downgrading instructions to EC (without extensions for some instructions)
+print("Is mac: " + str(IS_MAC) + " and arch is " + str(machine_arch))
 if IS_MAC and machine_arch == "rv32ecxw":
     machine_arch = "rv32ec"
 
