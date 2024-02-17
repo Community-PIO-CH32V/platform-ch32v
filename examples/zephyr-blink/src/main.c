ad71d3892002f01f5bebb5d2838b94ceda4a1733
maxgerhardt <maximilian.gerhardt@rub.de>
2024-02-17 15:46:43 +0100

    Working Zephyr blink and print example

--- examples/zephyr-blink/src/main.c
+++ examples/zephyr-blink/src/main.c
@@ -3,20 +3,27 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS 300
+#define SLEEP_TIME_MS 1000
 
-/* The devicetree node identifier for the "led0" alias. */
-//#define LED0_NODE DT_ALIAS(led0)
+/* The devicetree node identifier for the "led_custom" alias.
+   This pin is being defined in zephyr/boards/usb_pdmon.overlay as PB1.
+   Can also use the following aliases
+   led0 = ledr = PA7 
+   led1 = ledb = PB3 
+   led2 = ledg = PB0 
+ */
+#define LED_NODE DT_ALIAS(ledcustom)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-//static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+static unsigned cnt = 0;
 
 void main(void)
 {
-#if 0
 	int ret;
 
 	if (!gpio_is_ready_dt(&led))
@@ -29,16 +36,16 @@ void main(void)
 	{
 		return;
 	}
-#endif
 	while (1)
 	{
-        #if 0
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0)
 		{
 			return;
 		}
-        #endif
-		k_msleep(SLEEP_TIME_MS);
+		/* UART output on PA2 at 115200 baud */
+		/* per overlay file */
+		printk("Toggled LED! %u\n", cnt++);
+ 		k_msleep(SLEEP_TIME_MS);
 	}
 }
