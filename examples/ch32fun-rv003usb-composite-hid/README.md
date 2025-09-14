8a26a8e85460a3473cae71eb904e3cd7be391ad2
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2025-09-14 18:31:38 +0200

    Add rv003usb example

---
+++ examples/ch32fun-rv003usb-composite-hid/README.md
@@ -0,0 +1,26 @@
+How to build PlatformIO based project
+=====================================
+
+See <https://github.com/cnlohr/rv003usb/tree/master/demo_composite_hid>
+
+1. [Install PlatformIO Core](https://docs.platformio.org/page/core.html)
+2. Download [development platform with examples](https://github.com/Community-PIO-CH32V/platform-ch32v/archive/develop.zip)
+3. Extract ZIP archive
+4. Run these commands:
+
+```shell
+# Change directory to example
+$ cd platform-ch32v/examples/ch32fun-rv003usb-composite-hid
+
+# Build project
+$ pio run
+
+# Upload firmware
+$ pio run --target upload
+
+# Upload firmware for the specific environment
+$ pio run -e ch32v003f4p6_evt_r0 --target upload
+
+# Clean build files
+$ pio run --target clean
+```
---
+++ examples/ch32fun-rv003usb-composite-hid/src/demo_composite_hid.c
@@ -0,0 +1,82 @@
+#include "ch32fun.h"
+#include <stdio.h>
+#include <string.h>
+#include "rv003usb.h"
+
+int main()
+{
+	SystemInit();
+	// funGpioInitC();
+	// funPinMode(PC5, GPIO_CFGLR_OUT_10Mhz_PP);
+	Delay_Ms(1); // Ensures USB re-enumeration after bootloader or reset; Spec demand >2.5µs ( TDDIS )
+	usb_setup();
+	while(1)
+	{
+		// funDigitalWrite(PC5, 1);
+		// Delay_Ms(1000);
+		// funDigitalWrite(PC5, 0);
+		// Delay_Ms(1000);
+#if RV003USB_EVENT_DEBUGGING
+		uint32_t * ue = GetUEvent();
+		if( ue )
+		{
+			printf( "%lu %lx %lx %lx\n", ue[0], ue[1], ue[2], ue[3] );
+		}
+#endif
+	}
+}
+
+void usb_handle_user_data( struct usb_endpoint * e, int current_endpoint, uint8_t * data, int len, struct rv003usb_internal * ist )
+{
+	if (len > 0) {
+		LogUEvent(1139, data[0], 0, current_endpoint);
+	}
+}
+
+void usb_handle_user_in_request( struct usb_endpoint * e, uint8_t * scratchpad, int endp, uint32_t sendtok, struct rv003usb_internal * ist )
+{
+	if( endp == 1 )
+	{
+		// Mouse (4 bytes)
+		static int i;
+		static uint8_t tsajoystick[4] = { 0x00, 0x00, 0x00, 0x00 };
+		i++;
+		int mode = i >> 5;
+
+		// Move the mouse right, down, left and up in a square.
+		switch( mode & 3 )
+		{
+		case 0: tsajoystick[1] =  1; tsajoystick[2] = 0; break;
+		case 1: tsajoystick[1] =  0; tsajoystick[2] = 1; break;
+		case 2: tsajoystick[1] = -1; tsajoystick[2] = 0; break;
+		case 3: tsajoystick[1] =  0; tsajoystick[2] =-1; break;
+		}
+		usb_send_data( tsajoystick, 4, 0, sendtok );
+	}
+	else if( endp == 2 )
+	{
+		// Keyboard (8 bytes)
+		static int i;
+		static uint8_t tsajoystick[8] = { 0x00 };
+		usb_send_data( tsajoystick, 8, 0, sendtok );
+
+		i++;
+
+		// Press a Key every second or so.
+		if( (i & 0x7f) == 0 )
+		{
+			tsajoystick[4] = 0x05; // 0x05 = "b"; 0x53 = NUMLOCK; 0x39 = CAPSLOCK;
+		}
+		else
+		{
+			tsajoystick[4] = 0;
+		}
+	}
+	else
+	{
+		// If it's a control transfer, empty it.
+		usb_send_empty( sendtok );
+	}
+}
+
+
