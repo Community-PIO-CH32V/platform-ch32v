878855df77919c34e071532dba7014e7c0fb9560
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2026-03-06 03:18:26 +0900

    add UIAPduino Pro micro CH32V003 v1.4, 2
    add UIAPduino Pro micro CH32V003 v1.4, 2

--- misc/scripts/gen_boarddefs.py
+++ misc/scripts/gen_boarddefs.py
@@ -274,6 +274,15 @@ known_boards: List[KnownBoard] = [
                             }
                         }
                    }),
+    KnownBoard("UIAPduino_Pro_micro_CH32V003_v1dot4", "UIAPduino Pro micro CH32V003 v1.4", get_chip("CH32V003F4P6"),
+            "https://www.uiap.jp/uiapduino/pro-micro/ch32v003/v1dot4", "UIAP, Umeta & Ikki Automotive Parts", {
+                    "build.arduino": {
+                        "openwch": {
+                            "variant": "CH32V00x/CH32V003F4",
+                            "variant_h": "variant_CH32V003F4.h"
+                        }
+                    }
+                }),
     KnownBoard("suzuno32rv", "BitTradeOne Suzuno32RV", get_chip("CH32V203C8T6"),
                "https://www.github.com/verylowfreq/board_suzuno32rv", "BitTradeUno", {
                        "build.arduino": {
